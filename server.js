fde92d2de08cf0936ebeb8992692978738eb5e6b
Hiroshi MORIMOTO <hiroshi.morimoto@metamoji.com>
2023-06-04 15:25:50 +0900

    リファクタリングとコメント修正

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+//利用モジュール
 const os = require('os');
 const fs = require('fs')
 const http = require('http')
-const WebSocketServer = require('websocket').server
+const websocketServer = require('websocket').server
 
+//定数
 const [EN0] = Object.values(os.networkInterfaces())
 const {address: ADDRESS} = EN0.find(({family}) => family === 'IPv4') //サーバーのIPアドレス
 const PORT = 3000 //httpサーバーのポート
 const PROTOCOL = 'my-chat' //WebSocketのプロトコル識別子
 
-// httpサーバーのrequestハンドラ
-const server = http.createServer((request, response) => {
+//httpサーバーのrequestハンドラ
+const g_httpServer = http.createServer((request, response) => {
     const url = request.url
     switch (url) {
         case '/':
@@ -25,20 +27,20 @@ const server = http.createServer((request, response) => {
     }
 })
 
-// httpサーバーを起動
-server.listen(PORT, () => {
+//httpサーバーを起動
+g_httpServer.listen(PORT, () => {
     console.log(`${new Date()} listen http://${ADDRESS}:${PORT}`)
 })
 
-// WebSocketサーバーをhttpサーバーに寄生させる
-const wsServer = new WebSocketServer({
-    httpServer: server,
+//WebSocketサーバーをhttpサーバーに寄生させる
+const g_websocketServer = new websocketServer({
+    httpServer: g_httpServer,
     autoAcceptConnections: false //cross-origin protectionを無効化しない
 })
 
-// WebSocketサーバーのrequestハンドラ
-wsServer.on('request', (request) => {
-    // originの検査
+//WebSocketサーバーのrequestハンドラ
+g_websocketServer.on('request', (request) => {
+    //originの検査
     console.log(`${new Date()} check origin: ${request.origin}`)
     if (request.origin !== `http://localhost:${PORT}` && request.origin !== `http://${ADDRESS}:${PORT}`) {
         request.reject()
@@ -46,15 +48,15 @@ wsServer.on('request', (request) => {
         return
     }
 
+    //コネクション確立とイベントハンドラ
     const connection = request.accept(PROTOCOL, request.origin)
     console.log(`${new Date()} acceepted: ${connection.remoteAddress}`)
-
     connection.on('message', message => {
         switch (message.type) {
             case 'utf8':
                 console.log(`${new Date()} text message: ${message.utf8Data}`)
-                //connection.sendUTF(message.utf8Data) //送信元だけに送る
-                wsServer.broadcast(message.utf8Data) //全端末に送る
+                //connection.sendUTF(message.utf8Data) //送信元だけに送る場合
+                g_websocketServer.broadcast(message.utf8Data) //全端末に送る場合
                 break
             case 'binary':
                 console.log(`${new Date()} binary message: ${message.binaryData.length}byte`)
@@ -62,7 +64,6 @@ wsServer.on('request', (request) => {
                 break
         }
     })
-
     connection.on('close', (reasonCode, description) => {
         console.log(`${new Date()} closed: ${connection.remoteAddress}`)
     })
